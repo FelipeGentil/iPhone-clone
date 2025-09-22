787ffca1c2d2e11b1cea8419a27dc9829ed13566
Felipe Gentil <rgentil344@gmail.com>
2025-09-22 18:08:54 -0300

    alteração js

--- JS/script.js
+++ JS/script.js
@@ -3,18 +3,15 @@ const image = document.querySelector(".main-content__product-image");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    const button = e.currentTarget; // garante que sempre seja a LI clicada
+    const button = e.currentTarget; 
     const color = button.getAttribute("data-color");
 
-    // Remove seleção anterior
     buttons.forEach((b) =>
       b.querySelector(".image-picker__color").classList.remove("image-picker__color--selected")
     );
 
-    // Adiciona seleção na cor clicada
     button.querySelector(".image-picker__color").classList.add("image-picker__color--selected");
 
-    // Anima troca de imagem
     image.classList.add("changing");
     image.setAttribute("src", `img/iphone_${color}.jpg`);
     setTimeout(() => {
